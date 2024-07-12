428dd210fed818c97b132af351e11a08a3753cae
Natan Mendes <natancmendes@gmail.com>
2024-07-11 21:46:18 -0300

    addd functions on app.js

--- a-projeto-base/app.js
+++ a-projeto-base/app.js
@@ -1,8 +1,18 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+//exemplos de códigos que devem ser alterados para o código abaixo
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+// devemos utilizar funções para evitar códigos repetidos
+function exibirTextoNaTela() {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     console.log('O botão foi clicado!')
