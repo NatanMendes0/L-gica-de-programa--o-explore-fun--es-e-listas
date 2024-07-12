98de2aac5ceada66a165fbfc0b99a3d9ea9a6f89
Natan Mendes <natancmendes@gmail.com>
2024-07-12 18:00:50 -0300

    update verificarChute

--- a-projeto-base/app.js
+++ a-projeto-base/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 //exemplos de códigos que devem ser alterados para o código abaixo
 // let titulo = document.querySelector('h1');
@@ -13,24 +14,28 @@ function exibirTextoNaTela() {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+//Código omitido
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
+
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+        tentativas++;
+
     }
 }
 
-//exemplo de função de retorno de número aleatório
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
+    //exemplo de função de retorno de número aleatório
+    function gerarNumeroAleatorio() {
+        return parseInt(Math.random() * 10 + 1);
+    }
